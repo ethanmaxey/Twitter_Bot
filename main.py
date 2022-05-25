3cf86f1f239b18aee538f6a36f5bc4936b92423d
ethan <ethanm113@gmail.com>
2022-05-25 13:13:35 -0500

    Added data and context arguments to main() in hopes of fix

---
+++ main.py
@@ -0,0 +1,51 @@
+import tweepy
+from datetime import datetime
+
+
+def main():
+    consumer_key = "2EQJgGiNgazoZSYexBRUZogIq"
+    consumer_secret = "i4YQtSUv3P2g5T8JYn2r98fgQJ0DUUF7W4EFahkUSDgmSCNdGw"
+    key = "1528924144016007169-qLbYhy2RAVnuYCG40rZVZjxIoRaqc0"
+    secret = "I3D3IlizESMw3uUkDas4L3AhbpcyMBkcmb5Vh0O7aDZPY"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(key, secret)
+    api = tweepy.API(auth)
+
+    triggers = ["radical democrats", "impeach biden", "joe biden's america", "biden's america", "nazi's", "nazi",
+                "illegals", "regime", "trans-terrorism", "trump", "left-wing", "the establishment", "the far left",
+                "wacktivism", "president trump", "taking our guns", "lgb", "let's go brandon", "lets go brandon",
+                "targeted conservatives", "the left", "higher than hunter", "sleepy joe", "me too'd", "wall",
+                "build a wall", "america first", "dems", "fauci", "christianity", "woke politics", "woke",
+                "stolen from president trump", "not telling the truth", "election integrity", "gas prices",
+                "president trump", "return to god", "rino", "border", "constitutional conservative",
+                "great replacement", "jab", "radical left", "gestapo", "gazpacho", "biden administration",
+                "fascist", "antifa", "bidenflation", "aliens"]
+
+    following = tweepy.Cursor(api.get_friends).items()
+    count = 1
+
+    for friend in following:
+        statuses = api.user_timeline(screen_name=friend.screen_name, count=count, since_id=count,
+                                     tweet_mode="extended")
+
+        for status in statuses:
+            exact_tweet_time = status._json['created_at']
+            formatted_datetime = datetime.strftime(datetime.strptime(exact_tweet_time,
+                                                                     '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d')
+            current_time = datetime.date(datetime.today())
+
+            contains = any(element in status.full_text.lower() for element in triggers)  # Produces boolean
+
+            if str(formatted_datetime) == str(current_time) and contains:
+                try:
+                    api.retweet(status.id)
+                    api.update_status(status='cringe', in_reply_to_status_id=status.id,
+                                      auto_populate_reply_metadata=True)
+                    print(f"Retweeting {friend.name}:\n {status.full_text}")
+                except tweepy.errors.Forbidden:
+                    print(f"\nALREADY RETWEETED {friend.name}'s IGNORANT TWEET")
+
+
+if __name__ == "__main__":
+    main()
