a0f7ea473f920784fb654268830534b4ed0d01bf
ethan <ethanm113@gmail.com>
2022-05-25 13:21:06 -0500

    Added comments for clarity

--- cloud.py
+++ cloud.py
@@ -17,10 +17,12 @@ def main():
     following = tweepy.Cursor(api.get_friends).items()
     count = 1
 
+    # Iterates over all the people the bot follows
     for friend in following:
         statuses = api.user_timeline(screen_name=friend.screen_name, count=count, since_id=count,
                                      tweet_mode="extended")
 
+        # Iterates over each most recent tweet of each person I follow
         for status in statuses:
             exact_tweet_time = status._json['created_at']
             formatted_datetime = datetime.strftime(datetime.strptime(exact_tweet_time,
@@ -29,6 +31,7 @@ def main():
 
             contains = any(element in status.full_text.lower() for element in triggers)  # Produces boolean
 
+            # If the date posted is today and there is a trigger word in the tweet, do the following
             if str(formatted_datetime) == str(current_time) and contains:
                 try:
                     api.retweet(status.id)
